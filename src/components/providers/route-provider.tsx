9bb5923aeb47086931726fa52927d7aaba65b263
Kaique Aguilar <contact@kaiqueaguilar.dev>
2025-06-10 12:37:00 -0300

    fix: correct import path for NotFoundScreen in route provider

--- src/components/providers/route-provider.tsx
+++ src/components/providers/route-provider.tsx
@@ -1,9 +1,9 @@
 import { query } from "@/components/providers/query-provider";
 import { ErrorScreen } from "@/components/screens/error-screen";
+import { NotFoundScreen } from "@/components/screens/not-found-screen";
 import { PendingScreen } from "@/components/screens/pending-screen";
 import { routeTree } from "@/lib/gen/route-tree.gen";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { NotFoundScreen } from "../screens/not-found-screen";
 
 export const router = createRouter({
   routeTree,
